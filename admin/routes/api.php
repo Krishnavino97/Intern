c6943d9e71b8596e62f5be53813017ee2947a273
Vinoji Sureshkumar <70386403+Krishnavino97@users.noreply.github.com>
2022-04-03 11:53:01 +0530

    DCA_admin_login & signup

---
+++ admin/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+//admin
+Route::post("addadmin",[AdminController::class,'addAdmin']);
+Route::get("login",[AdminController::class,'login']);
+Route::put("changePassword",[AdminController::class,'changePassword']);
+Route::post("forgetPasswordOTP",[AdminController::class,'forgetPasswordOTP']);
+Route::put("forgetPasswordOTPValidate",[AdminController::class,'forgetPasswordOTPValidate']);
+Route::get("viewadmin",[AdminController::class,'viewAdmin']);
+Route::post("updateadmin",[AdminController::class,'updateAdmin']);
+
+
